c88cadb936603c3e211bbbf514e73097c56742d8
Suu-hyun <suhyeonseong@gmail.com>
2025-04-01 17:55:38 +0900

    feat:day16

--- day16.py
+++ day16.py
@@ -164,5 +164,12 @@
 try:
     my_dict = {1: "사과", 2: "바나나", 3: "딸기", 4: "포도", 5: "수박"}
     dict_key = int(input("숫자를 입력 : "))
-except IndexError:
-    print("해당 키는 존재하지않습니다.")
+    result = my_dict[dict_key]
+except KeyError:
+    print("해당 키는 존재하지 않습니다.")
+except ValueError:
+    print("숫자를 입력해주세요")
+else:
+    print(result)
+finally:
+    print("완료")
